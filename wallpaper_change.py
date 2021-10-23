638d9f51f0570abe8aa0ca273304dc425da6776f
Nutikass <62213946+Nutikass@users.noreply.github.com>
2021-10-23 16:33:15 +0200

    Update wallpaper_change.py

--- wallpaper_change.py
+++ wallpaper_change.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-r = requests.get("https://wallpaper.dog/large/10876450.jpg", stream=True);
+r = requests.get("https://wallpaper.dog/large/10876450.jpg", stream=True)
 
 with open("wppr.png", 'wb') as wp:
     for chunk in r.iter_content(chunk_size=1024):
