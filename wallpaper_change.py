742ccf483a5634b83788e3236b540a396a1e2ede
Nutikas <aniakslowik@gmail.com>
2021-10-23 16:17:42 +0200

    initial commit

---
+++ wallpaper_change.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+r = requests.get("https://wallpaper.dog/large/10876450.jpg", stream=True);
+
+with open("wppr.png", 'wb') as wp:
+    for chunk in r.iter_content(chunk_size=1024):
+        if not chunk:
+            break
+
+        wp.write(chunk)
+
+time.sleep(1.5)
